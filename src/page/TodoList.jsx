2e7cead9c35d97b67958f661f89b7a4e24faa473
kouuch <rifkyirsada2@gmail.com>
2025-09-12 16:41:29 +0800

    tpe string

--- src/page/TodoList.jsx
+++ src/page/TodoList.jsx
@@ -5,39 +5,22 @@ const TodoList = () => {
   return (
     <div className="w-full mt-100">
       <p>Number Now : {count} </p>
-      
       <div>
-        {count % 2 === 0 ? (
-          <p className="text-green-500">Genap</p>
+        {typeof count === "number" ? (
+          count % 2 === 0 ? (
+            <p>Genap</p>
+          ) : (
+            <p>Ganjil</p>
+          )
         ) : (
-          <p className="text-red-500">Ganjil</p>
+          <p>Bukan Angka</p>
         )}
       </div>
-      <div>
-        {count < 0 && <p className="text-red-500">Negatif</p>}
-        {count === 0 && <p className="text-yellow-500">Nol</p>}
-        {count > 0 && <p className="text-green-500">Positif</p>}
+      <div className="border shadow flex gap-4">
+        <button onClick={() => setCount(count + 1)}>Tambah +1</button>
+        <button onClick={() => setCount(count - 1)}>Kurang -1</button>
+        <button onClick={() =>  setCount("string bre")}>String</button>
       </div>
-      <div>
-        {count >= 10 && <p className="text-blue-500">Angka sudah diatas 10</p>}
-        {count <= -10 && (
-          <p className="text-blue-500">Angka sudah dibawah -10</p>
-        )}
-      </div>
-      <div>
-        {count > 0 && count < 10 && (
-          <p className="text-purple-500">Angka diantara 0 sampai 10</p>
-        )}
-        {count < 0 && count > -10 && (
-          <p className="text-purple-500">Angka diantara -10 sampai 0</p>
-        )}
-      </div>
-      <div>
-        {count === 7 && <p className="text-pink-500">Angka Tujuh</p>}
-        {count === -7 && <p className="text-pink-500">Angka Minus Tujuh</p>}
-      </div>
-      <button onClick={() => setCount(count + 1)}>Tambah +1</button>
-      <button onClick={() => setCount(count - 1)}>Kurang -1</button>
     </div>
   );
 };
