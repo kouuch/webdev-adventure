62675ac0cdc2f45eeed5ebb8e9e3357a70b4af54
kouuch <rifkyirsada2@gmail.com>
2025-09-14 23:00:04 +0800

    props

--- src/page/TodoList.jsx
+++ src/page/TodoList.jsx
@@ -1,25 +1,18 @@
 import { useState } from "react";
+import Card from "../components/Card";
 
 const TodoList = () => {
-  const [user, setUser] = useState({
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  });
+  const title = "Todo List";  
+  const description = [
+    "Belajar React",
+    "Mengerjakan tugas",
+    "Berolahraga",
+    "Membaca buku",
+  ];
 
-  const updateUser = () => {
-    
-    setUser({
-      name: 'Kouch', 
-      age: 30, 
-    });
-  };
   return (
     <div className="w-full mt-100">
-      <p>Name: {user.name}</p>
-      <p>Age: {user.age}</p>
-      <p>Email: {user.email}</p>
-      <button onClick={updateUser}>Update Name</button>
+      <Card title={title} description={description}/>
     </div>
   );
 };
