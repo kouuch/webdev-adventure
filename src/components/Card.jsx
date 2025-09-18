7d057e340a3a7bf0417a427e81b4572244de7fca
kouuch <rifkyirsada2@gmail.com>
2025-09-18 11:59:33 +0800

    fungsi tambah selesai dan hapus

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,132 +3,160 @@ import Form from "./Form";
 
 const Card = () => {
   const [open, setOpen] = useState(null);
-  const todoList = [
-    { title: "Belajar React", description: "Mempelajari dasar-dasar React.js" },
+  //1. Buat state untuk menampung data todo list
+  const [task, setTask] = useState([
+    {
+      title: "Belajar",
+      description: "Mempelajari dasar-dasar React.js",
+      isComplate: false,
+    },
     {
       title: "Mengerjakan tugas",
       description: "Menyelesaikan tugas-tugas kuliah",
+
+      isCompleted: false,
     },
     {
       title: "Berolahraga",
-      description:
-        "Melakukan aktivitas fisik untuk kesehatan lorem ipsum  Melakukan aktivitas fisik untuk kesehatan lorem ipsum Melakukan aktivitas fisik untuk kesehatan lorem ipsum",
+      description: "Melakukan aktivitas fisik untuk kesehatan",
+      isCompleted: false,
     },
     {
       title: "Membaca buku",
       description: "Membaca buku-buku pengembangan diri",
+      isCompleted: false,
     },
-  ];
+  ]);
 
-  const todoDone = [
-    { title: "Belajar React", description: "Mempelajari dasar-dasar React.js" },
-    {
-      title: "Mengerjakan tugas",
-      description: "Menyelesaikan tugas-tugas kuliah",
-    },
-    {
-      title: "Berolahraga",
-      description:
-        "Melakukan aktivitas fisik untuk kesehatan lorem ipsum  Melakukan aktivitas fisik untuk kesehatan lorem ipsum Melakukan aktivitas fisik untuk kesehatan lorem ipsum",
-    },
-  ];
+  //2. Buat state untuk menampung data todo list yang selesai
+  const [completedTasks, setCompletedTasks] = useState([]);
+
+  //3. menambahkan tugas baru ke dalam daftar tugas
+  const addTask = (newTask) => {
+    setTask([...task, newTask]);
+  };
+
+  //4. menghapus tugas dari daftar tugas
+  const removeTask = (index) => {
+    const updateTask = task.filter((_, i) => i !== index);
+    setTask(updateTask);
+  };
+
+  //5. menandai tugas sebagai selesai
+  const taskDone = (index) => {
+    const doneTask = task[index];
+    setCompletedTasks([...completedTasks, doneTask]);
+    removeTask(index);
+  };
+
   return (
     <>
       <h1 className="text-3xl text-center text-gray-700 font-semibold">
         Todo List
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-20 mt-4">
-        <Form />
+        <Form addTask={addTask} />
         <div className="w-full mx-auto flex flex-col items-center  px-4  md:px-0">
           <h3>Daftar Tugas</h3>
-          {todoList.map((item, index) => (
-            <div
-              key={index}
-              className="border-b border-slate-200 cursor-pointer w-full"
-              onClick={() => setOpen(open === index ? null : index)}
-            >
-              <div className="flex justify-between items-center py-2">
-                <h3 className="text-base font-medium">{item.title}</h3>
-                <div className=" flex gap-2">
-                  <button className="border rounded-lg py-1.2 px-2 font-medium bg-green-500 text-white border-none hover:bg-green-700 cursor-pointer">
-                    Selesai
-                  </button>
-                  <button className="border rounded-lg py-1.2 px-2 font-medium bg-red-500 text-white border-none hover:bg-red-700 cursor-pointer">
-                    Hapus
-                  </button>
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`${
-                      open === index ? "rotate-180" : ""
-                    } transition-all ease-in-out duration-300 `}
-                  >
-                    <path
-                      d="m4.5 7.2 3.793 3.793a1 1 0 0 0 1.414 0L13.5 7.2"
-                      stroke="#1D293D"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <p
-                className={`text-sm text-gray-500 max-w-md transition-all ease-in-out duration-500 ${
-                  open === index
-                    ? "opacity-100 max-h-[300px] -translate-y-0 pt-4"
-                    : "opacity-0 max-h-0 -translate-y-2"
-                }`}
+          {task.map((item, index) => (
+            <div key={index} className="mt-2 cursor-pointer w-full flex gap-2">
+              <div
+                className="border-b border-slate-200 w-full"
+                onClick={() => setOpen(open === index ? null : index)}
               >
-                {item.description}
-              </p>
+                <div className="flex justify-between items-center py-2">
+                  <h3 className="text-base font-medium">{item.title}</h3>
+                  <div className=" flex gap-2">
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`${
+                        open === index ? "rotate-180" : ""
+                      } transition-all ease-in-out duration-300 `}
+                    >
+                      <path
+                        d="m4.5 7.2 3.793 3.793a1 1 0 0 0 1.414 0L13.5 7.2"
+                        stroke="#1D293D"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p
+                  className={`text-sm text-gray-500 max-w-md transition-all ease-in-out duration-500 ${
+                    open === index
+                      ? "opacity-100 max-h-[300px] -translate-y-0 pt-4"
+                      : "opacity-0 max-h-0 -translate-y-2"
+                  }`}
+                >
+                  {item.description}
+                </p>
+              </div>
+              <button
+                onClick={() => taskDone(index)}
+                className="border rounded-lg py-1.2 px-2 font-medium bg-green-500 text-white border-none hover:bg-green-700 cursor-pointer"
+              >
+                Selesai
+              </button>
+              <button
+                onClick={() => removeTask(index)}
+                className="border rounded-lg py-1.2 px-2 font-medium bg-red-500 text-white border-none hover:bg-red-700 cursor-pointer"
+              >
+                Hapus
+              </button>
             </div>
           ))}
           <h3 className=" mt-4">Tugas Selesai</h3>
-          {todoDone.map((item, index) => (
-            <div
-              key={index}
-              className="border-b border-slate-200 cursor-pointer w-full"
-              onClick={() => setOpen(open === index ? null : index)}
-            >
-              <div className="flex justify-between items-center py-2">
-                <h3 className="text-base font-medium">{item.title}</h3>
-                <div className="flex gap-2">
-                  <button className="border rounded-lg py-1.2 px-2 font-medium bg-red-500 text-white border-none hover:bg-red-700 cursor-pointer">
-                    Hapus
-                  </button>
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`${
-                      open === index ? "rotate-180" : ""
-                    } transition-all ease-in-out duration-300 `}
-                  >
-                    <path
-                      d="m4.5 7.2 3.793 3.793a1 1 0 0 0 1.414 0L13.5 7.2"
-                      stroke="#1D293D"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
-              </div>
-              <p
-                className={`text-sm text-gray-500 max-w-md transition-all ease-in-out duration-500 ${
-                  open === index
-                    ? "opacity-100 max-h-[300px] -translate-y-0 pt-4"
-                    : "opacity-0 max-h-0 -translate-y-2"
-                }`}
+          {completedTasks.map((item, index) => (
+            <div key={index} className="mt-2 cursor-pointer w-full flex gap-2">
+              <div
+                className="border-b border-slate-200 w-full"
+                onClick={() => setOpen(open === index ? null : index)}
               >
-                {item.description}
-              </p>
+                <div className="flex justify-between items-center py-2">
+                  <h3 className="text-base font-medium">{item.title}</h3>
+                  <div className=" flex gap-2">
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`${
+                        open === index ? "rotate-180" : ""
+                      } transition-all ease-in-out duration-300 `}
+                    >
+                      <path
+                        d="m4.5 7.2 3.793 3.793a1 1 0 0 0 1.414 0L13.5 7.2"
+                        stroke="#1D293D"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p
+                  className={`text-sm text-gray-500 max-w-md transition-all ease-in-out duration-500 ${
+                    open === index
+                      ? "opacity-100 max-h-[300px] -translate-y-0 pt-4"
+                      : "opacity-0 max-h-0 -translate-y-2"
+                  }`}
+                >
+                  {item.description}
+                </p>
+              </div>
+              <button
+                onClick={() => removeTask(index)}
+                className="border rounded-lg py-1.2 px-2 font-medium bg-red-500 text-white border-none hover:bg-red-700 cursor-pointer"
+              >
+                Hapus
+              </button>
             </div>
           ))}
         </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 const Form = ({ addTask }) => {
   //1. Buat state untuk menampung data form
@@ -6,18 +6,21 @@ const Form = ({ addTask }) => {
   const [description, setDescription] = useState("");
 
   //2. Buat fungsi untuk handle perubahan pada input form
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     //3.Buat validasi input form
     if (title && description) {
-      addTask({ title, description });
+      addTask({ title, description, isComplate: false });
       setTitle("");
       setDescription("");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-white w-full mx-auto rounded-lg p-4 text-left text-gray-500 text-sm border border-gray-300/60">
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white w-full mx-auto rounded-lg p-4 text-left text-gray-500 text-sm border border-gray-300/60"
+    >
       <label htmlFor="content" className="font-medium">
         Title
       </label>
@@ -25,7 +28,10 @@ const Form = ({ addTask }) => {
         type="text"
         id="title"
         value={title}
+        onChange={(e) => setTitle(e.target.value)}
         className="w-full mt-2 mb-4 border border-gray-500/30 outline-none rounded py-2 px-3"
+        placeholder="Masukkan title"
+        required
       />
       <label htmlFor="content" className="font-medium">
         Detail
@@ -36,7 +42,9 @@ const Form = ({ addTask }) => {
         cols="30"
         rows="10"
         value={description}
-        className="w-full mt-2 border border-gray-500/30 outline-none rounded resize-none"
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Masukkan detail"
+        className="w-full mt-2 border border-gray-500/30 outline-none rounded resize-none py-2 px-3"
       ></textarea>
       <div className="flex justify-center mt-3 mb-2">
         <button
